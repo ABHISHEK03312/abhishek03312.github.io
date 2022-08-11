0635d6f242a84dfa3cee53733aa9aff39b842529
Abhishek Vaidyanathan <abhishekvaidyanathan@Abhisheks-MacBook-Pro-2.local>
2022-08-11 21:10:17 +0800

    tab name changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import './css-styles.css'
 import Navbar from './navbar';
 import About from './about';
+import React,{useState,useEffect} from 'react';
 import { BrowserRouter , Route,Routes} from 'react-router-dom';
 import Work from './workExperience';
 import Profile from './profile';
 import Projects from './projects';
 
 function App() {
+  const [title, setTitle] = useState(" Abhi | Portfolio");
+
+  useEffect(() => {
+    // This will run when the page first loads and whenever the title changes
+    document.title = title;
+  }, [title]);
   return (
     <div className='mainApp'>
       <div className="App">
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
               <Route path='/' element={<Profile/>}/>
-              {/* <Route path='/'/> */}
               <Route path='/workexperience' element={<Work />}/>
               <Route path='/projects' element={<Projects />} />
               <Route path='/about' element={<About/>} />
--- src/profile.js
+++ src/profile.js
@@ -29,17 +29,9 @@ const Profile= () => {
     return (
         <div className="profile"> 
             <div className="profile-sub-content">
-                {/* <div className="profile-pic">
-                    <img src = {ProfilePictureCopy} alt="Abhishek" className="profile-img" />
-                </div> */}
                 <div className="profile-content">
                     <div className="profile-content-text">
                         <p className="profile-para-code-p">
-                            {/* <pre className="profile-para-code">
-                                >>I'm Abhishek Vaidyanathan valbopadmvadmvkdsmvsd
-                            </pre> */}
-                            {/* - I'm Abhishek Vaidyanathan */}
-                            -
                             <Typewriter
                             options={{
                                 strings: ["I'm Abhishek Vaidyanathan ", "I'm a Developer","I'm a Data scientist"],
@@ -51,39 +43,6 @@ const Profile= () => {
                         <p className="profile-para">
                          A final year student studying at Nanyang Technological University. Passionate in Data science, I am also a football fanatic. I believe in giving it my best shot in eveything I do.
                         </p>
-                        {/* <div>
-                            <div className="contact-links">
-                                <h3 className="profile-link-headers">
-                                    Name:  
-                                </h3>
-                                <p className="profile-link-text"> Abhishek Vaidyanathan</p>
-                            </div>
-                            <div className="contact-links">
-                                <h3 className='profile-link-headers'>
-                                    Email:
-                                </h3>
-                                <p><a href='mailto:vaidyanathanabhishek@gmail.com' className="profile-links">
-                                vaidyanathanabhishek@gmail.com</a>
-                                </p>
-                            </div>
-                            <div className="contact-links">
-                                <h3 className="profile-link-headers">
-                                    Contact:  
-                                </h3>
-                                <p className="profile-link-text"> +65 85755238 </p>
-                            </div>
-                            <div className="social-icons">
-                                <h3 className="profile-link-headers">
-                                    Social:
-                                </h3>
-                                <div className="social-indv-icons">
-                                    <SocialIcon url="https://www.linkedin.com/in/abhishek-vaidyanathan-3364b2196/" style={{ height: 40, width: 40 }}/>
-                                </div>
-                                <div className="social-indv-icons">
-                                    <SocialIcon url="https://github.com/ABHISHEK03312" bgColor="rgb(255, 255, 255)" style={{ height: 40, width: 40 }}/>
-                                </div>
-                            </div>
-                        </div> */}
                     </div>
                     <div className="resume-button-div">
                         <a href={MyPDF} download="AbhishekVaidyanthanResume.pdf" style={{'text-decoration':'none'}}>
